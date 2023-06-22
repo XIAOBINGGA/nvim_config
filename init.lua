f0d9133603c5e0faa7d93c07cb5554d5709b7546
wangchhs <wangchhs@yonyou.com>
2023-06-22 11:18:27 +0800

    添加配置文件

--- init.lua
+++ init.lua
@@ -1,2 +1,10 @@
 -- 基础配置
-require("basic")
+require('basic')
+-- 添加包管理器
+require('lua.plugs')
+-- 添加主题
+-- require('lua.colorscheme')
+-- 插件配置
+require('lua.plugs_config.nvim-tree')
+
+print("init ok")
