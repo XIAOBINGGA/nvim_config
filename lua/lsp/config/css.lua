f8dcf36789fc10f9ad1d938690c85cb57dae2271
wangchhs <wangchhs@yonyou.com>
2023-07-03 13:40:07 +0800

    配置htmllsp和csslsp

---
+++ lua/lsp/config/css.lua
@@ -0,0 +1,35 @@
+local opts = {
+  -- capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+  flags = {
+    debounce_text_changes = 150,
+  },
+  settings = {
+    css = {
+      validate = true,
+    },
+    less = {
+      validate = true,
+    },
+    scss = {
+      validate = true,
+    },
+  },
+  on_attach = function(client, bufnr)
+    local function buf_set_keymap(...)
+      -- vim.notify("加载 lspconfig keybing 1")
+      vim.api.nvim_buf_set_keymap(bufnr, ...)
+    end
+    -- 绑定快捷键
+    require('keybindings').mapLSP(buf_set_keymap)
+
+    -- 禁用格式化功能，交给专门插件插件处理
+    -- client.resolved_capabilities.document_formatting = false
+    -- client.resolved_capabilities.document_range_formatting = false
+  end
+}
+
+return {
+  on_setup = function(server)
+    server.setup(opts)
+  end
+}
