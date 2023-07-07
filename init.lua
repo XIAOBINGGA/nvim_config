0810e9d47e32059cb6110dc2368033963ab44d29
wangchhs <wangchhs@yonyou.com>
2023-07-07 14:44:05 +0800

    添加lazy.nvim

--- init.lua
+++ init.lua
@@ -1,37 +1,15 @@
--- 基础配置
-require('basic')
--- 添加包管理器
-require('plugs')
--- 添加主题
-require('colorscheme')
--- -- bufferline
--- require('plugs_config.buffer-line')
--- lualine
-require('plugs_config.lua-line')
--- telescope
-require('plugs_config.telescope')
--- 添加project项目管理
-require('plugs_config.project')
--- 插件配置
-require('plugs_config.nvim-tree')
--- treeitter
-require('plugs_config.nvim-treesitter')
--- which-key
-require('plugs_config.which-key')
--- lsp 配置
-require('lsp.setup')
--- 补全引擎
-require('lsp.cmp')
--- lsp ui 增强
-require('lsp.ui')
---- null-ls 
-require('lsp.null-ls')
--- 注释插件
-require('plugs_config.comment')
--- 快速移动插件
-require('plugs_config.leap')
--- surround 插件
-require('plugs_config.surround')
--- 括号自动补全插件
-require('plugs_config.autopairs')
--- print("init ok")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({
+  "folke/which-key.nvim",
+})
--- lua/plugs_config/which-key.lua
+++
@@ -1,8 +0,0 @@
-local status, whickKey = pcall(require, "which-key")
-
-if not status then
-    vim.notify("没有找到 which-key")
-    return
-end
-
-whickKey.setup({})
