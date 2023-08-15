a52d7106f90611f980c92924cde570d142980fe9
wangchhs <wangchhs@yonyou.com>
2023-08-15 19:49:53 +0800

    添加新的插件

--- lua/plugins/line.lua
+++ lua/plugins/line.lua
@@ -1,16 +1,15 @@
-local tools = require('tools')
 return {
-	tools.url_replace('nvim-lualine/lualine.nvim'),
-	dependencies = {
-		tools.url_replace('nvim-tree/nvim-web-devicons'),
-	},
-	config = function()
-		require('lualine').setup({
+  'nvim-lualine/lualine.nvim',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+  },
+  config = function()
+    require('lualine').setup({
       options = {
         component_separators = { left = "|", right = "|" },
         -- https://github.com/ryanoasis/powerline-extra-symbols
         section_separators = { left = " ", right = "" },
       }
     })
-	end
+  end
 }
