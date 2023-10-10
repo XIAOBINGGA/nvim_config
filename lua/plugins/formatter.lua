6795a18f7aad6d9a23e811baff87842c77cf15db
wangchhs <wangchhs@yonyou.com>
2023-10-10 13:18:17 +0800

    添加ts格式化文件

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -31,6 +31,33 @@ return {
             }
           end
         },
+        typescript = {
+          function(parser)
+            if not parser then
+              return {
+                exe = "prettier",
+                args = {
+                  "--stdin-filepath",
+                  util.escape_path(util.get_current_buffer_file_path()),
+                },
+                stdin = true,
+                try_node_modules = true,
+              }
+            end
+
+            return {
+              exe = "prettier",
+              args = {
+                "--stdin-filepath",
+                util.escape_path(util.get_current_buffer_file_path()),
+                "--parser",
+                parser,
+              },
+              stdin = true,
+              try_node_modules = true,
+            }
+          end
+        },
         javascript = {
           function(parser)
             if not parser then
