672eb7e041c483365956ec0dfb48cbea4902f3fa
wangchhs <wangchhs@yonyou.com>
2023-12-19 17:14:45 +0800

    feat: 🎸 添加字体图标文件

--- lua/tools.lua
+++ lua/tools.lua
@@ -1,23 +1,61 @@
--- 分割字符串
-local function split(str, reps)
-  local resultStrList = {}
-  string.gsub(str, '[^' .. reps .. ']+', function(w)
-    table.insert(resultStrList, w)
-  end)
-  return resultStrList
-end
-
 local tools = {}
-tools.url_replace = function(url)
-  return url
-  -- if url then
-  --   local data = split(url, '/')
-  --   if data then
-  --     data[1] = 'XIAOBINGGA/'
-  --     return table.concat(data)
-  --   end
-  --   return url
-  -- end
-  -- return url
-end
+tools.symbols = {
+    Affirmative = "✓ ",
+    Array = " ",
+    Boolean = " ",
+    Class = "󰠱 ",
+    CodeAction = " ",
+    Color = "󰏘 ",
+    Component = "󰡀 ",
+    Constant = "󰏿 ",
+    Constructor = " ",
+    Definition = " ",
+    Diagnostic = " ",
+    Dice1 = "󰇊 ",
+    Dice2 = "󰇋 ",
+    Dice3 = "󰇌 ",
+    Dice4 = "󰇍 ",
+    Dice5 = "󰇎 ",
+    Dice6 = "󰇏 ",
+    Dos = " ",
+    Enum = " ",
+    EnumMember = " ",
+    Error = " ",
+    Event = " ",
+    Field = "󰜢 ",
+    File = "󰈙 ",
+    Folder = "󰉋 ",
+    Fragment = " ",
+    Function = "󰊕 ",
+    Hint = " ",
+    Info = " ",
+    Interface = " ",
+    Key = " ",
+    Keyword = "󰌋 ",
+    LspSagaFinder = " ",
+    Mac = " ",
+    Method = "󰆧 ",
+    Module = "󰆧 ",
+    Namespace = " ",
+    Negative = "✗ ",
+    Null = "󰟢 ",
+    Number = " ",
+    Object = " ",
+    Operator = "󰆕 ",
+    Package = " ",
+    Pending = "➜ ",
+    Property = "󰜢 ",
+    Reference = "󰈇 ",
+    RenamePrompt = "➤ ",
+    Snippet = " ",
+    String = " ",
+    Struct = "󰙅 ",
+    Text = "󰉿 ",
+    TypeParameter = "󰉺 ",
+    Unit = "󰑭 ",
+    Unix = " ",
+    Value = "󰎠 ",
+    Variable = "󰀫 ",
+    Warn = " ",
+}
 return tools
