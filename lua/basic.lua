bdb1923deb41afd317a902758ecef16b65536ec1
wangchhs <wangchhs@yonyou.com>
2023-12-19 16:53:40 +0800

    feat: 🎸 调整basic文件

--- lua/basic.lua
+++ lua/basic.lua
@@ -1,9 +1,9 @@
--- -- utf8
+-- utf8
 vim.g.encoding = "UTF-8"
 vim.o.fileencoding = 'utf-8'
--- -- jkhl 移动时光标周围保留8行
--- vim.o.scrolloff = 8
--- vim.o.sidescrolloff = 8
+-- jkhl 移动时光标周围保留8行
+vim.o.scrolloff = 8
+vim.o.sidescrolloff = 8
 -- 使用相对行号
 -- vim.wo.number = true
 vim.wo.relativenumber = true
@@ -31,21 +31,20 @@ vim.o.smartindent = true
 -- 搜索大小写不敏感，除非包含大写
 vim.o.ignorecase = true
 vim.o.smartcase = true
--- -- 搜索不要高亮
--- vim.o.hlsearch = true
+-- 搜索不要高亮
+vim.o.hlsearch = true
 -- -- 边输入边搜索
 -- vim.o.incsearch = true
 -- 命令行高为2，提供足够的显示空间
 -- vim.o.cmdheight = 1
--- -- 当文件被外部程序修改时，自动加载
--- vim.o.autoread = true
--- vim.bo.autoread = true
--- 禁止折行
+-- 当文件被外部程序修改时，自动加载
+vim.o.autoread = true
+vim.bo.autoread = true -- 禁止折行
 vim.wo.wrap = true
--- -- 光标在行首尾时<Left><Right>可以跳到下一行
--- vim.o.whichwrap = '<,>,[,]'
--- -- 允许隐藏被修改过的buffer
--- vim.o.hidden = false
+-- 光标在行首尾时<Left><Right>可以跳到下一行
+vim.o.whichwrap = '<,>,[,]'
+-- 允许隐藏被修改过的buffer
+vim.o.hidden = false
 -- -- 鼠标支持
 -- -- vim.o.mouse = "a"
 -- 禁止创建备份文件
@@ -56,11 +55,11 @@ vim.o.swapfile = false
 -- vim.o.updatetime = 300
 -- -- 设置 timeoutlen 为等待键盘快捷键连击时间500毫秒，可根据需要设置
 -- vim.o.timeoutlen = 500
--- -- split window 从下边和右边出现
--- vim.o.splitbelow = true
--- vim.o.splitright = true
--- -- 自动补全不自动选中
--- vim.g.completeopt = "menu,menuone,noselect,noinsert"
+-- split window 从下边和右边出现
+vim.o.splitbelow = true
+vim.o.splitright = true
+-- 自动补全不自动选中
+vim.g.completeopt = "menu,menuone,noselect,noinsert"
 -- 样式
 vim.o.background = "light"
 vim.o.termguicolors = true
@@ -70,8 +69,8 @@ vim.opt.termguicolors = true
 -- vim.o.listchars = "space:·"
 -- 补全增强
 vim.o.wildmenu = true
--- -- Dont' pass messages to |ins-completin menu|
--- vim.o.shortmess = vim.o.shortmess .. 'c'
+-- Dont' pass messages to |ins-completin menu|
+vim.o.shortmess = vim.o.shortmess .. 'c'
 -- 补全最多显示10行
 vim.o.pumheight = 10
 -- -- 永远显示 tabline
