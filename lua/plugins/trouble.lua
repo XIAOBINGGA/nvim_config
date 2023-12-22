0470aee27823fe7c1f96f50a239a7e0fd056fb35
wangchhs <wangchhs@yonyou.com>
2023-12-22 14:44:03 +0800

    feat: 🎸 添加trouble提示插件

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,6 @@
+return {
+  "folke/trouble.nvim",
+  keys = {
+    { "<leader>lt", ":TroubleToggle<CR>", desc = "trouble toggle", silent = true, noremap = true },
+  },
+}
--- test/js.js
+++ test/js.js
@@ -1,10 +1,4 @@
 let abc = 55;
-let abc1 = 55;
-let abc2 = 55;
-let abc23 = 55;
-let abc31 = 55;
-let abc213 = 55;
-let abcad = 55;
 function add() {
   if (void 0 === true) {
     if (1 == 1) {
