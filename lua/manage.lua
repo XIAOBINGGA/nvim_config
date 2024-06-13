a90cd7125224a98beb0c2ee7ba1ff34a05ecd6e2
wangchhs <wangchhs@yonyou.com>
2024-06-13 09:49:15 +0800

    feat: 🎸 组件升级

--- lua/manage.lua
+++ lua/manage.lua
@@ -16,7 +16,7 @@ require("lazy").setup({ { import = "plugins" } }, {
     -- defaults for the `Lazy log` command
     -- log = { "-10" }, -- show the last 10 commits
     log = { "-8" }, -- show commits from the last 3 days
-    timeout = 120,  -- kill processes that take more than 2 minutes
+    -- timeout = 120,  -- kill processes that take more than 2 minutes
     url_format = "git@github.com:%s.git",
     -- lazy.nvim requires git >=2.19.0. If you really want to use lazy with an older version,
     -- then set the below to false. This should work, but is NOT supported and will
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,45 @@
+return {
+  'nvim-telescope/telescope.nvim', tag = '0.1.6',
+  event = "VeryLazy",
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    -- 'nvim-telescope/telescope-file-browser.nvim',
+    -- 'nvim-telescope/telescope-project.nvim',
+  },
+  keys = {
+    { "<leader>tf", mode = { "n" }, function() require('telescope.builtin').find_files() end, desc = "find files" },
+    { "<leader>tg", mode = { "n" }, function() require('telescope.builtin').live_grep() end, desc = "live grep" },
+    { "<leader>tb", mode = { "n" }, function() require('telescope.builtin').buffers() end, desc = "buffers" },
+    { "<leader>th", mode = { "n" }, function() require('telescope.builtin').help_tags() end, desc = "help tags" },
+  },
+  config = function()
+    local status_telescope, telescope = pcall(require, "telescope")
+    if not status_telescope then
+      vim.notify("没有找到 telescope")
+      return
+    end
+    -- telescope.setup({
+    --   extensions = {
+    --     file_browser = {
+    --       -- theme = "ivy",
+    --       hijack_netrw = true,
+    --     },
+    --     project = {
+    --       base_dirs = {
+    --         '~/src',
+    --       },
+    --       hidden_files = true, -- default: false
+    --       theme = "dropdown",
+    --       order_by = "recent",
+    --       search_by = {"path", "title"},
+    --       sync_with_nvim_tree = false, -- default false 
+    --     },
+    --   },
+    --   defaults = {
+    --     -- mappings = require('keybings').telescopeList
+    --   }
+    -- })
+    -- pcall(telescope.load_extension, 'file_browser')
+    -- pcall(telescope.load_extension, "project")
+  end
+}
