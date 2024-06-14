dc4fbd6feb5c6f4fd8ce52d784a1038e74117b71
wangchhs <wangchhs@yonyou.com>
2024-06-14 09:31:47 +0800

    添加skip标签

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -10,20 +10,38 @@ return {
     'L3MON4D3/LuaSnip',
     'saadparwaiz1/cmp_luasnip',
     -- 片段提供
-    'rafamadriz/friendly-snippets'
+    'rafamadriz/friendly-snippets',
+    -- 添加图标
+    'onsails/lspkind.nvim',
   },
   event = { "InsertEnter", "CmdlineEnter" },
   config = function()
     local feedkey = function(key, mode)
       vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
     end
-
     local has_words_before = function()
       local line, col = unpack(vim.api.nvim_win_get_cursor(0))
       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
     end
+    local lspkind = require('lspkind')
     local cmp = require("cmp")
     cmp.setup({
+    formatting = {
+        format = lspkind.cmp_format({
+          mode = 'symbol', -- show only symbol annotations
+          maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                        -- can also be a function to dynamically calculate max width such as 
+                        -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+          ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+          show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+          -- The function below will be called before any actual modifications from lspkind
+          -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+          before = function (entry, vim_item)
+            return vim_item
+          end
+        })
+      },
       -- 快捷键绑定
       mapping = {
         -- 上一个
