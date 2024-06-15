022b0da376282d942abb6f259d67373513e4f1b8
wangchhs <wangchhs@yonyou.com>
2024-06-15 15:53:11 +0800

    feat: 🎸 部分调整

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -26,43 +26,54 @@ return {
       local lspconfig = require("lspconfig")
       local masonlspconfig = require("mason-lspconfig")
       masonlspconfig.setup({
-        ensure_installed = {
-          "lua_ls",
-        },
+        ensure_installed = { "lua_ls", "tsserver", "cssls", "html", "jsonls", "rescriptls" },
       })
-      masonlspconfig.setup_handlers({
-        ["lua_ls"] = function()
-          -- code
-          lspconfig.lua_ls.setup({
-            settings = {
-              Lua = {
-                runtime = {
-                  -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                  version = "LuaJIT",
-                  -- Setup your lua path
-                  path = runtime_path,
-                },
-                diagnostics = {
-                  -- Get the language server to recognize the `vim` global
-                  globals = { "vim" },
-                },
-                workspace = {
-                  -- Make the server aware of Neovim runtime files
-                  library = vim.api.nvim_get_runtime_file("", true),
-                  checkThirdParty = false,
-                },
-                -- Do not send telemetry data containing a randomized but unique identifier
-                telemetry = {
-                  enable = false,
-                },
-              },
-            },
-            flags = {
-              debounce_text_changes = 150,
-            },
-          })
-        end,
-      })
+      local setup_handlers = {}
+      for name, config in pairs(servers) do
+        if config ~= nil and type(config) == "table" then
+          if name == "dartls" then
+          -- config.on_setup(lspconfig[name])
+          else
+            setup_handlers[name] = function()
+              config.on_setup(lspconfig[name])
+            end
+          end
+        end
+      end
+      require("mason-lspconfig").setup_handlers(setup_handlers)
+      -- masonlspconfig.setup_handlers({
+      --   ["lua_ls"] = function()
+      --     -- code
+      --     lspconfig.lua_ls.setup({
+      --       settings = {
+      --         Lua = {
+      --           runtime = {
+      --             -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+      --             version = "LuaJIT",
+      --             -- Setup your lua path
+      --             path = runtime_path,
+      --           },
+      --           diagnostics = {
+      --             -- Get the language server to recognize the `vim` global
+      --             globals = { "vim" },
+      --           },
+      --           workspace = {
+      --             -- Make the server aware of Neovim runtime files
+      --             library = vim.api.nvim_get_runtime_file("", true),
+      --             checkThirdParty = false,
+      --           },
+      --           -- Do not send telemetry data containing a randomized but unique identifier
+      --           telemetry = {
+      --             enable = false,
+      --           },
+      --         },
+      --       },
+      --       flags = {
+      --         debounce_text_changes = 150,
+      --       },
+      --     })
+      --   end,
+      -- })
     end,
   },
 }
