33c771750b6d2413569f9d8176c6c22573ad1b8d
wangchhs <wangchhs@yonyou.com>
2024-08-26 17:42:58 +0800

    调整line样式

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -3,7 +3,7 @@ return {
   event = "VeryLazy",
   opts = {
     presets = {
-      bottom_search = true,
+      bottom_search = false,
       command_palette = true, -- position the cmdline and popupmenu together
       long_message_to_split = true, -- long messages will be sent to a split
     },
--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -24,7 +24,7 @@ return {
         "typescript",
         "tsx",
       },
-      -- auto_install = true,
+      auto_install = true,
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
