41ee7afa81ca7470d30b640fd54e1b9793838801
wangchhs <wangchhs@yonyou.com>
2024-08-28 17:19:16 +0800

    feat: 🎸 调整treesitter启用范围

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -24,20 +24,21 @@ return {
         "typescript",
         "tsx",
       },
-      auto_install = true,
+      -- auto_install = true,
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
-        disable = { "dart" },
-      },
-      rainbow = {
-        enable = true,
-        -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-        disable = { "dart" },
-        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-        max_file_lines = nil, -- Do not enable for files with more than n lines, int
-        -- colors = {}, -- table of hex strings
-        -- termcolors = {} -- table of colour name strings
+        -- disable = { "dart" },
+        disable = function(lang, buf)
+          if lang == "dart" then
+            return true
+          end
+          local max_filesize = 300 * 1024 -- 1kb = 1024
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
+        end,
       },
       incremental_selection = {
         enable = true,
