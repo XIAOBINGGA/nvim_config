43d042f6df7a8b7999d5bf236fb748bc90916c65
wangchhs <wangchhs@yonyou.com>
2024-09-03 10:00:47 +0800

    修改底部搜索

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -4,7 +4,7 @@ return {
   enabled = true,
   opts = {
     presets = {
-      bottom_search = false,
+      bottom_search = true,
       command_palette = true, -- position the cmdline and popupmenu together
       long_message_to_split = true, -- long messages will be sent to a split
     },
