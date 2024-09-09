c6101189e245e318fbd665bc0ae1e95d16b33fa6
wangchhs <wangchhs@yonyou.com>
2024-09-09 13:29:53 +0800

    更换模板插件

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,3 +1,13 @@
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
 return {
   "hrsh7th/nvim-cmp",
   enabled = require("tools").isenable(1),
@@ -8,8 +18,11 @@ return {
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-nvim-lsp",
     -- For luasnip users.
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
+    -- "L3MON4D3/LuaSnip",
+    -- "saadparwaiz1/cmp_luasnip",
+    --  For vsnip users.
+    "hrsh7th/cmp-vsnip",
+    "hrsh7th/vim-vsnip",
     -- 片段提供
     "rafamadriz/friendly-snippets",
     -- 添加图标
@@ -17,7 +30,6 @@ return {
   },
   event = "VeryLazy",
   config = function()
-    local luasnip = require("luasnip")
     local lspkind = require("lspkind")
     local cmp = require("cmp")
     cmp.setup({
@@ -60,49 +72,54 @@ return {
         -- ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
         ["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
         ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
+        -- luasnip
         -- ["<Tab>"] = cmp.mapping(function(fallback)
         --   if cmp.visible() then
         --     cmp.select_next_item()
-        --   elseif vim.fn["vsnip#available"](1) == 1 then
-        --     feedkey("<Plug>(vsnip-expand-or-jump)", "")
-        --   elseif has_words_before() then
-        --     cmp.complete()
+        --   elseif luasnip.expand_or_jumpable() then
+        --     luasnip.expand_or_jump()
         --   else
-        --     fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+        --     fallback()
         --   end
         -- end, { "i", "s" }),
-        -- ["<S-Tab>"] = cmp.mapping(function()
+        -- ["<S-Tab>"] = cmp.mapping(function(fallback)
         --   if cmp.visible() then
         --     cmp.select_prev_item()
-        --   elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-        --     feedkey("<Plug>(vsnip-jump-prev)", "")
+        --   elseif luasnip.jumpable(-1) then
+        --     luasnip.jump(-1)
+        --   else
+        --     fallback()
         --   end
         -- end, { "i", "s" }),
+        --vsnip
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif vim.fn["vsnip#available"](1) == 1 then
+            feedkey("<Plug>(vsnip-expand-or-jump)", "")
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+          end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function()
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+            feedkey("<Plug>(vsnip-jump-prev)", "")
+          end
+        end, { "i", "s" }),
       },
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
         -- 以下插件作为前提：
         -- { 'L3MON4D3/LuaSnip' },
         -- { 'saadparwaiz1/cmp_luasnip' },
-        { name = "luasnip" },
+        -- { name = "luasnip" },
+        -- vsnip
+        { name = "vsnip" },
         { name = "buffer" },
         { name = "path" },
       }),
@@ -123,6 +140,6 @@ return {
       matching = { disallow_symbol_nonprefix_matching = false },
     })
     -- load vscode snippet (friendly-snippet)
-    require("luasnip.loaders.from_vscode").lazy_load()
+    -- require("luasnip.loaders.from_vscode").lazy_load()
   end,
 }
