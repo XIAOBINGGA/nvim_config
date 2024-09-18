6e8fbf6a13585510e6e00f70d6a2568c35be4588
wangchhs <wangchhs@yonyou.com>
2024-09-18 11:03:43 +0800

    调整markdown插件

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
+### 更新说明
+
+**2024-9-18**
+
+- 更新markdown插件
+- 修改undotree插件快捷键 **leader u** -> **leader u t**
+
 
--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -3,7 +3,6 @@ return {
     "tpope/vim-markdown",
     event = "VeryLazy",
     config = function()
-      -- tpope/vim-markdown
       vim.g.markdown_syntax_conceal = 0
       vim.g.markdown_fenced_languages = {
         "html",
@@ -25,6 +24,7 @@ return {
   },
   {
     "yelog/marklive.nvim",
+    event = "VeryLazy",
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     lazy = true,
     ft = "markdown",
@@ -33,10 +33,41 @@ return {
   {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    build = "cd app && npm install",
+    build = "npm install",
+    event = "VeryLazy",
     init = function()
       vim.g.mkdp_filetypes = { "markdown" }
     end,
     ft = { "markdown" },
   },
+  {
+    "dhruvasagar/vim-table-mode",
+    event = "VeryLazy",
+    config = function()
+      vim.cmd(
+        [[
+        augroup markdown_config
+          autocmd!
+          autocmd FileType markdown nnoremap <buffer> <M-s> :TableModeRealign<CR>
+        augroup END
+      ]],
+        false
+      )
+      vim.g.table_mode_sort_map = "<leader>mts"
+    end,
+  }, --> table mode
+  {
+    "bullets-vim/bullets.vim",
+    event = "VeryLazy",
+    config = function()
+      vim.g.bullets_enabled_file_types = { "markdown", "text", "gitcommit", "scratch" }
+    end,
+  }, --> list style
+  {
+    "tenxsoydev/vim-markdown-checkswitch",
+    event = "VeryLazy",
+    config = function()
+      vim.g.md_checkswitch_style = "cycle"
+    end,
+  }, --> checkbox shortcut
 }
