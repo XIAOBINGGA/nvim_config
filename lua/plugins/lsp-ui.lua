f22f6cfba5c7931c6454c9fc4c08019c54235e89
wangchhs <wangchhs@yonyou.com>
2024-09-23 10:50:58 +0800

    调整行诊断信息显示方式

--- lua/plugins/lsp-ui.lua
+++ lua/plugins/lsp-ui.lua
@@ -3,7 +3,14 @@ return {
   enabled = require("tools").isenable(1),
   event = "BufEnter",
   config = function()
-    require("lspsaga").setup({})
+    vim.diagnostic.config({
+      virtual_text = false,
+    })
+    require("lspsaga").setup({
+      diagnostic = {
+        diagnostic_only_current = true,
+      },
+    })
   end,
   dependencies = {
     "nvim-treesitter/nvim-treesitter", -- optional
