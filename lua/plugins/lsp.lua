ceafcd76f9c87632f51b47b34fd56d479bd97ab9
wangchhs <wangchhs@yonyou.com>
2024-09-24 09:34:16 +0800

    html添加快捷键

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -30,7 +30,7 @@ return {
       local lspconfig = require("lspconfig")
       local masonlspconfig = require("mason-lspconfig")
       masonlspconfig.setup({
-        ensure_installed = { "lua_ls", "cssls", "html", "jsonls", "rescriptls", "marksman" },
+        ensure_installed = { "ts_ls", "lua_ls", "cssls", "html", "jsonls", "rescriptls", "marksman" },
       })
       local setup_handlers = {}
       for name, config in pairs(servers) do
@@ -45,39 +45,6 @@ return {
         end
       end
       require("mason-lspconfig").setup_handlers(setup_handlers)
-      -- masonlspconfig.setup_handlers({
-      --   ["lua_ls"] = function()
-      --     -- code
-      --     lspconfig.lua_ls.setup({
-      --       settings = {
-      --         Lua = {
-      --           runtime = {
-      --             -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-      --             version = "LuaJIT",
-      --             -- Setup your lua path
-      --             path = runtime_path,
-      --           },
-      --           diagnostics = {
-      --             -- Get the language server to recognize the `vim` global
-      --             globals = { "vim" },
-      --           },
-      --           workspace = {
-      --             -- Make the server aware of Neovim runtime files
-      --             library = vim.api.nvim_get_runtime_file("", true),
-      --             checkThirdParty = false,
-      --           },
-      --           -- Do not send telemetry data containing a randomized but unique identifier
-      --           telemetry = {
-      --             enable = false,
-      --           },
-      --         },
-      --       },
-      --       flags = {
-      --         debounce_text_changes = 150,
-      --       },
-      --     })
-      --   end,
-      -- })
     end,
   },
 }
