d443f2077bcfd2f29ee50c53f34bc47d17b5b6c8
wangchhs <wangchhs@yonyou.com>
2025-02-14 09:39:38 +0800

    feat: 🎸 添加自动保存

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -18,3 +18,23 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     vim.b.autoformat = false
   end,
 })
+
+local function save()
+  local buf = vim.api.nvim_get_current_buf()
+
+  vim.api.nvim_buf_call(buf, function()
+    vim.cmd("silent! write")
+  end)
+end
+
+vim.api.nvim_create_augroup("AutoSave", {
+  clear = true,
+})
+
+vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+  callback = function()
+    save()
+  end,
+  pattern = "*",
+  group = "AutoSave",
+})
