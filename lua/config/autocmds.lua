82bcfde53c068c892549ddf27afd0381aa1d7e92
wangchhs <wangchhs@yonyou.com>
2025-04-18 16:44:45 +0800

    调整自动保存自动格式化方法

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -18,22 +18,22 @@
 --   end,
 -- })
 
--- local function save()
---   local buf = vim.api.nvim_get_current_buf()
---
---   vim.api.nvim_buf_call(buf, function()
---     vim.cmd("silent! write")
---   end)
--- end
---
--- vim.api.nvim_create_augroup("AutoSave", {
---   clear = true,
--- })
---
--- vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
---   callback = function()
---     save()
---   end,
---   pattern = "*",
---   group = "AutoSave",
--- })
+local function save()
+  local buf = vim.api.nvim_get_current_buf()
+
+  vim.api.nvim_buf_call(buf, function()
+    vim.cmd("silent! write")
+  end)
+end
+
+vim.api.nvim_create_augroup("AutoSave", {
+  clear = true,
+})
+
+vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+  callback = function()
+    save()
+  end,
+  pattern = "*",
+  group = "AutoSave",
+})
