a9d4fc2034e714b2123d23c24417d14405b25c08
wangchhs <wangchhs@yonyou.com>
2025-08-08 13:44:39 +0800

    添加自定义按键

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,3 +8,8 @@ local opt = {
   noremap = true,
   silent = true,
 }
+
+    vim.keymap.set("n", "<leader>b", [[<cmd>lua require('vscode').action('workbench.action.toggleSidebarVisibility')<cr>]])
+    vim.keymap.set("n", "<leader>e", [[<cmd>lua require('vscode').action('workbench.view.explorer')<cr>]])
+    vim.keymap.set("n", "<leader>z", [[<cmd>lua require('vscode').action('workbench.action.toggleZenMode')<cr>]])
+    vim.keymap.set("n", "<leader>t", [[<cmd>lua require('vscode').action('workbench.action.terminal.toggleTerminal')<cr>]])
